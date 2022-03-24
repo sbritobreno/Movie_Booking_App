8aad49016e6eab627315ba20d59c6ed98a9bc2c5
sbritobreno <breno.silvabrito'hotmail.com>
2022-03-24 19:02:33 +0000

    git repository and empty activity created

---
+++ app/src/main/java/ie/dorset/ID24059/ca2_movieBookingApp/MainActivity.kt
@@ -0,0 +1,11 @@
+package ie.dorset.ID24059.ca2_movieBookingApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
