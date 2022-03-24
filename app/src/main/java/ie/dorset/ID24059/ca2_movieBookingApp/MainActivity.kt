ea125a416b1fe545fc124882d316b6421b75c9c7
sbritobreno <breno.silvabrito'hotmail.com>
2022-03-24 23:38:18 +0000

    recyclerView implemented

--- app/src/main/java/ie/dorset/ID24059/ca2_movieBookingApp/MainActivity.kt
+++ app/src/main/java/ie/dorset/ID24059/ca2_movieBookingApp/MainActivity.kt
@@ -2,10 +2,24 @@ package ie.dorset.ID24059.ca2_movieBookingApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter:RecyclerView.Adapter<MovieAdapter.ViewHolder>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        layoutManager = LinearLayoutManager(this)
+
+        movies_recycler_view.layoutManager = layoutManager
+
+        adapter = MovieAdapter()
+        movies_recycler_view.adapter = adapter
     }
 }
---
+++ app/src/main/java/ie/dorset/ID24059/ca2_movieBookingApp/MovieAdapter.kt
@@ -0,0 +1,52 @@
+package ie.dorset.ID24059.ca2_movieBookingApp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MovieAdapter: RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
+
+    private var name = arrayOf("Into The Wild", "The Boy in the Striped Pajamas", "The Avengers", "Spider_Man 2")
+    private var starring = arrayOf("Emile Hirsch, Vince Vaughn", "Asa Butterfield, Jack Scanlon", "Robert Downey, Scarlett Johansson", "Tobey Maguire, Kirsten Dunst")
+    private var running_time_mins = arrayOf("2h 28m", "1h 34m", "2h 23m", "2h 7m")
+    private var seatsRemaining = arrayOf("15", "15", "15", "15")
+    private val image = intArrayOf(R.drawable.intothewildimg, R.drawable.theboyinthestripedpajamas, R.drawable.theavengers, R.drawable.spiderman2)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.movie_recycler_template,parent,false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: MovieAdapter.ViewHolder, position: Int) {
+        holder.name.text = name[position]
+        holder.starring.text = starring[position]
+        holder.running_time_mins.text = running_time_mins[position]
+        holder.seatsRemaining.text = seatsRemaining[position]
+        holder.image.setImageResource(image[position])
+    }
+
+    override fun getItemCount(): Int {
+        return name.size
+    }
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+        var name: TextView
+        var image: ImageView
+        var starring: TextView
+        var running_time_mins: TextView
+        var seatsRemaining: TextView
+
+        init{
+            name = itemView.findViewById(R.id.MovieTitle)
+            image = itemView.findViewById(R.id.MovieImage)
+            starring = itemView.findViewById(R.id.MovieStarring)
+            running_time_mins = itemView.findViewById(R.id.MovieDuration)
+            seatsRemaining = itemView.findViewById(R.id.NumberOfRemainingSeats)
+        }
+    }
+}
